8c25ec5d0386fa4c14718a2f5366ef8c4618b92c
vvaricca <30466198+vvaricca@users.noreply.github.com>
2019-08-22 13:38:00 +0200

    Update infos-stream.js

--- js/infos-stream.js
+++ js/infos-stream.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function() {
+	
+	$.getJSON('https://api.twitch.tv/kraken/channels/56985289?client_id=g6i4lxcedxo0ujc8hpmxhkx6ejd85j&callback=?', function(data) {
+		console.log(data.views);
+		console.log(data.status);
+		$("#streamTitle").text (data.status);
+		$("#nbVues").text (data.views);
+	});
+});
