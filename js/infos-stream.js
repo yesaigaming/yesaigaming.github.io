c690ceb48f0ca63b013ad0f53ed4c5eda70ea57e
vvaricca <30466198+vvaricca@users.noreply.github.com>
2019-09-18 16:14:35 +0200

    Nouvelle API Twitch

--- js/infos-stream.js
+++ js/infos-stream.js
@@ -1,10 +1,34 @@
-
 $(document).ready(function() {
 	
-	$.getJSON('https://api.twitch.tv/kraken/channels/56985289?client_id=g6i4lxcedxo0ujc8hpmxhkx6ejd85j&callback=?', function(data) {
-		console.log(data.views);
-		console.log(data.status);
-		$("#streamTitle").text (data.status);
-		$("#nbVues").text (data.views);
+	$.ajax({
+	  url: "https://api.twitch.tv/helix/streams?user_login=tox_yesai",
+	  dataType: 'json',
+	  type: 'GET',
+      beforeSend: function(xhr){xhr.setRequestHeader('Client-ID', 'g6i4lxcedxo0ujc8hpmxhkx6ejd85j');},
+	  success: function (data) {
+		    if (data.data.length > 0)
+			{
+				console.log(data.data[0].viewer_count);
+				console.log(data.data[0].title);
+				var status = data.data[0].title ;
+				var nbVues = data.data[0].viewer_count ;
+				$("#streamTitle").text (data.data[0].title);
+				$("#nbVues").text (data.data[0].viewer_count);
+			}
+			else 
+			{
+				$("#streamTitle").text ("Stream offline");
+				$.ajax({
+				  url: "https://api.twitch.tv/helix/users?login=tox_yesai",
+				  dataType: 'json',
+				  type: 'GET',
+				  beforeSend: function(xhr){xhr.setRequestHeader('Client-ID', 'g6i4lxcedxo0ujc8hpmxhkx6ejd85j');},
+				  success: function (data) {
+					  console.log(data.data[0].view_count);
+					  $("#nbVues").text (data.data[0].view_count);
+					}
+				});
+			}
+		}
 	});
 });
