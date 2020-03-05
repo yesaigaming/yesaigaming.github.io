fccf13120b2464e145b4e36701c8bbfb7c84e3dd
vvaricca <30466198+vvaricca@users.noreply.github.com>
2020-03-05 19:04:16 +0100

    Key update

--- js/infos-youtube.js
+++ js/infos-youtube.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var divListeYT = $("#listeYT");
 	
 	/* $.getJSON('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UULYGnYm-wn38ZxaGrPM4_ug&key=AIzaSyBcheOgcFfFJ0i7gZ_n5mkieLuhCPZuinE&part=contentDetails,snippet&maxResults=5&callback=?', function(data) { */
-	$.getJSON('https://www.googleapis.com/youtube/v3/search?type=video&order=date&channelId=UCLYGnYm-wn38ZxaGrPM4_ug&playlistId=UULYGnYm-wn38ZxaGrPM4_ug&key=AIzaSyBcheOgcFfFJ0i7gZ_n5mkieLuhCPZuinE&part=snippet&maxResults=5&callback=?', function(data) {
+	$.getJSON('https://www.googleapis.com/youtube/v3/search?type=video&order=date&channelId=UCLYGnYm-wn38ZxaGrPM4_ug&playlistId=UULYGnYm-wn38ZxaGrPM4_ug&key=AIzaSyAFRV-5Ytg4f8kLtfAe96-J6hsK49H_a_M&part=snippet&maxResults=5&callback=?', function(data) {
 		$.each( data.items, function(key,item) {
 			videoYT = [item.snippet.title, item.snippet.description, item.snippet.thumbnails.high.url, "https://www.youtube.com/watch?v="+item.id.videoId];
 			var l = videosYoutube.push (videoYT) ;
